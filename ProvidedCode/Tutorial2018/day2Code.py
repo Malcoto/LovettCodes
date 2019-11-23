bfcfe1467adae35c91482d596993d3eef687ece6
Malcoto <mkj7@rice.edu>
2019-11-22 20:36:04 -0600

    Finished day2Code in tutorial.

--- ProvidedCode/Tutorial2018/day2Code.py
+++ ProvidedCode/Tutorial2018/day2Code.py
@@ -3,9 +3,10 @@ import collections
 # TODO Read through the day 1 problem of 2018, then you can go through this file and try to implement the given
 # suggestions to make the code work! (Delete all TODOs when finished.)
 
+## TODO: Add to the input files.
 
 ## TODO Change file path to appropriate input file.
-INPUT_FILE_NAME = "Inputs/day2Input.txt"
+INPUT_FILE_NAME = "Inputs/dayXInput.txt"
 
 
 # Having the string is already good enough, so this function is a no-op. (No operation)
@@ -29,7 +30,7 @@ def has_x_repeats(x, id):
 
     ## TODO: For each letter in id, count it in repeats
     for letter in id:
-        repeats[letter] += 1
+        # repeats[letter] += ...
         continue
 
     # The method values returns a list of the values in the dictionary it is called on. For repeats this results in
@@ -40,7 +41,7 @@ def has_x_repeats(x, id):
     has_x = x in counts
 
     ## TODO: Return the boolean value of whether id has a letter repeated x times.
-    return has_x
+    # return ...
 
 
 ## TODO: Implement part 1 solution
@@ -50,59 +51,69 @@ def part_1():
     # Get a list of the integers representing the frequency changes from the input file.
     boxIds = parse_instructions(INPUT_FILE_NAME)
 
-    ## TODO: Initialize two counters for the number of ids with 2 or 3 repeated letters.
-    repeat_2 = 0
-    repeat_3 = 0
+    ## TODO: Initialize two counters for the number of ids with 2 or 3 repeated letters. (Start at 0)
+    # repeat_2 = ...
+    # repeat_3 = ...
 
     ## TODO: For each id in the list boxIds, increment the relevent counters.
     for boxId in boxIds:
         if has_x_repeats(2, boxId):
-            repeat_2 += 1
+            # repeat_2 += ...
+            pass ## TODO: Delete this line
         if has_x_repeats(3, boxId):
-            repeat_3 += 1
+            # repeat_3 += ...
+            pass ## TODO: Delete this line
 
     ## TODO: Calculate the checksum.
-    checksum = repeat_2 * repeat_3
+    # checksum = ...
 
 
-    ## TODO: Return the resultant frequency.
-    return checksum
+    ## TODO: Return the resultant checksum.
+    # return ...
+
+## TODO: Implement a function which counts the number of differences between two ids.
+def count_differences(id1, id2):
+    differences = 0
+    for letter1, letter2 in zip(id1, id2): # The zip function returns a list of pairs, so that letter1 and letter2 are from id1 and id2.
+        if letter1 != letter2:
+            # differences += ...
+            pass
+    return differences
+
+## TODO: Implement a function which returns the common letters between the two ids.
+def common_letters(id1, id2):
+    common = ""
+    # for letter1, letter2 in ...:
+    #     if letter1 == letter2:
+    #         common += letter1
+    # return common
 
 ## TODO: Implement part 2 solution
 def part_2():
     ## TODO: We want to find the two ids which differ by only 1 letter. It is inefficient to check every pair, so
-    ## we will start by sorting
+    ## we will start by sorting the ids alphabetically, so that only consecutive ids could be a valid pair.
 
     # Get a list of the integers representing the frequency changes from the input file.
-    integers = parse_instructions(INPUT_FILE_NAME)
+    boxIds = parse_instructions(INPUT_FILE_NAME)
 
-    # Initially start the frequency at 0.
-    frequency = 0
+    # Sort the box ids alphabetically.
+    sortedBoxIds = sorted(boxIds)
 
-    # Initialize an "empty set" called visitedFrequencies to keep track of which frequencies have been observed.
-    visitedFrequencies = set([])
+    # Initialize an index to store the matching in.
+    matchIdx = None
 
-    ## TODO: Add frequency to the set, since we already visited it.
-    # visitedFrequencies.add(...)
+    ## TODO Go through the ids in the sorted list, until two consecutive ids differ in only one location.
+    for idx in range(len(boxIds) - 1):
+        # if count_differences(sortedBoxIds[idx], sortedBoxIds[idx + 1]) == ...:
+        #     matchIdx = ...
+        #     break
+        pass ## TODO: Delete this file.
 
-    ## TODO: Once code is working delete return None statement. (It's there to prevent an infinite loop for now.)
-    return None
+    ## TODO: Initialize inCommon to be the common letters between the two relevent ids in sortedBoxIds.
+    # inCommon = ...
+    inCommon = None ## TODO: Delete this line.
 
-    while True:
-        ## TODO: For each frequency in the list integers, change frequency, and check if it has been
-        ## already encountered. Once the list is progressed entired through, the while loop will take us
-        ## back to the start.
-        for frequencyChange in integers:
-            frequency += frequencyChange
-            ## TODO: Return frequency if it has already been visited. (Remove continue once return is implemented)
-            if frequency in visitedFrequencies:
-                continue
-                #return ...
-            else:
-                ## TODO: Add frequency to visitedFrequencies. (Ditto with the continue)
-                continue
-                ## ...
-
+    return inCommon
 
 
 print("Part 1 answer: {}".format(part_1()))
