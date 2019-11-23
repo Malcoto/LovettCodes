d2f336f424171c356a17e0c595df7d05bedf2476
mby1 <michaelyoung2398@gmail.com>
2019-11-22 19:52:38 -0600

    This is Michael's second push

--- mby1/utils.py
+++ mby1/utils.py
@@ -5,3 +5,4 @@ def manhatten_distance(point_1, point_2):
     assert len(point_1) == len(point_2), "Error calculating manhatten distance. Points were of different dimensions."
     return sum([math.fabs(point_1[idx]-point_2[idx]) for idx in range(len(point_1))])
 
+print ('hi mak this is my push')
